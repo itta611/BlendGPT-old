cd14184fa414ed7750a221a53192c00833a18915
Itta Funahashi <itta@itta.dev>
2023-04-23 10:39:17 +0900

    feat: create class WebGLCanvasDrawer

--- components/WebGLCavas.tsx
+++ components/WebGLCavas.tsx
@@ -1,14 +1,19 @@
 import { FC, HTMLAttributes, useEffect, useRef } from 'react';
-import { drawWebGLCanvas } from 'utils/drawWebGLCanvas';
+import { WebGLCanvasDrawer } from 'utils/WebGLCanvasDrawer';
 
 interface WebGLCanvasProps extends HTMLAttributes<HTMLCanvasElement> {}
 
 const WebGLCanvas: FC<WebGLCanvasProps> = ({ ...props }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null!);
+  const canvasDrawerRef = useRef<WebGLCanvasDrawer>(null!);
 
   useEffect(() => {
-    drawWebGLCanvas(canvasRef.current, '/dummy.png');
-  });
+    (async () => {
+      canvasDrawerRef.current = new WebGLCanvasDrawer(canvasRef.current, '/dummy.png');
+      await canvasDrawerRef.current.init();
+      canvasDrawerRef.current.draw();
+    })();
+  }, []);
 
   return <canvas ref={canvasRef} {...props}></canvas>;
 };
---
+++ utils/WebGLCanvasDrawer.ts
@@ -0,0 +1,161 @@
+export class WebGLCanvasDrawer {
+  canvas: HTMLCanvasElement;
+  imageURL: string;
+  gl: WebGLRenderingContext;
+  vertexShaderSource: string;
+  fragmentShaderSource: string;
+  program: WebGLProgram;
+
+  constructor(canvas: HTMLCanvasElement, imageURL: string) {
+    this.canvas = canvas;
+    this.imageURL = imageURL;
+    this.gl = undefined!;
+    this.vertexShaderSource = `attribute vec4 a_position;
+attribute vec2 a_texCoord;
+varying vec2 v_texCoord;
+void main() {
+    gl_Position = a_position;
+    v_texCoord = a_texCoord;
+}`;
+
+    this.fragmentShaderSource = `precision mediump float;
+uniform sampler2D u_image;
+varying vec2 v_texCoord;
+void main() {
+  vec4 color = texture2D(u_image, v_texCoord);
+  gl_FragColor = color;
+}
+`;
+    this.program = undefined!;
+  }
+
+  private createShader(gl: WebGLRenderingContext, type: number, source: string) {
+    const shader = gl.createShader(type)!;
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+
+    if (success) return shader;
+
+    console.error(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+  }
+
+  private createProgram(
+    gl: WebGLRenderingContext,
+    vertexShader: WebGLShader,
+    fragmentShader: WebGLShader
+  ) {
+    const program = gl.createProgram()!;
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) return program;
+
+    console.error(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+  }
+
+  private loadImage(url: string) {
+    return new Promise<HTMLImageElement>((resolve, reject) => {
+      const image = new Image();
+      image.crossOrigin = 'anonymous';
+      image.src = url;
+      image.addEventListener('load', () => {
+        resolve(image);
+      });
+    });
+  }
+
+  private setupProgram() {
+    const vertexShader = this.createShader(this.gl, this.gl.VERTEX_SHADER, this.vertexShaderSource);
+    const fragmentShader = this.createShader(
+      this.gl,
+      this.gl.FRAGMENT_SHADER,
+      this.fragmentShaderSource
+    );
+    this.program = this.createProgram(this.gl, vertexShader!, fragmentShader!)!;
+  }
+
+  public async init() {
+    this.gl = (this.canvas.getContext('webgl') ||
+      this.canvas.getContext('experimental-webgl')) as WebGLRenderingContext;
+
+    if (!this.gl) {
+      throw new Error('WebGL renderer not initialized.');
+    }
+
+    this.setupProgram();
+
+    const positionBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
+    this.gl.bufferData(
+      this.gl.ARRAY_BUFFER,
+      new Float32Array([-1, -1, 1, -1, -1, 1, 1, 1]),
+      this.gl.STATIC_DRAW
+    );
+    const texCoordBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, texCoordBuffer);
+    this.gl.bufferData(
+      this.gl.ARRAY_BUFFER,
+      new Float32Array([0, 1, 1, 1, 0, 0, 1, 0]),
+      this.gl.STATIC_DRAW
+    );
+
+    const positionAttributeLocation = this.gl.getAttribLocation(this.program, 'a_position');
+    this.gl.enableVertexAttribArray(positionAttributeLocation);
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
+    this.gl.vertexAttribPointer(positionAttributeLocation, 2, this.gl.FLOAT, false, 0, 0);
+
+    const texCoordAttributeLocation = this.gl.getAttribLocation(this.program, 'a_texCoord');
+    this.gl.enableVertexAttribArray(texCoordAttributeLocation);
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, texCoordBuffer);
+    this.gl.vertexAttribPointer(texCoordAttributeLocation, 2, this.gl.FLOAT, false, 0, 0);
+
+    const image = await this.loadImage(this.imageURL);
+
+    this.canvas.width = image.width;
+    this.canvas.height = image.height;
+
+    const texture = this.gl.createTexture();
+    this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+    this.gl.texImage2D(
+      this.gl.TEXTURE_2D,
+      0,
+      this.gl.RGBA,
+      this.gl.RGBA,
+      this.gl.UNSIGNED_BYTE,
+      image
+    );
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+  }
+
+  public updateFragmentShader(shader: string) {
+    this.fragmentShaderSource = shader;
+    this.draw();
+  }
+
+  public draw() {
+    this.gl.clearColor(0, 0, 0, 1);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+
+    this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+
+    this.gl.useProgram(this.program);
+
+    const resolutionUniformLocation = this.gl.getUniformLocation(this.program, 'u_resolution');
+    this.gl.uniform2f(resolutionUniformLocation, this.canvas.width, this.canvas.height);
+
+    const radiusUniformLocation = this.gl.getUniformLocation(this.program, 'u_radius');
+    this.gl.uniform1f(radiusUniformLocation, 5.0); // Increase this value for more blur
+
+    this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
+  }
+}
