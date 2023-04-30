830b1098524b21039b555e522e43d87f28354805
Itta Funahashi <itta@itta.dev>
2023-04-30 16:05:46 +0900

    feat: update prompt

--- utils/systemPrompt.ts
+++ utils/systemPrompt.ts
@@ -1,5 +1,7 @@
-const systemPrompt = `Create the fragment shader code, outputting any variables besides the default uniform variables u_image, u_resolution, and v_texCoord that must be passed to params.
-Be sure to remove information other than JSON.
+const systemPrompt = `You are the AI that edit images.
+Create the fragment shader code, outputting any variables besides the default uniform variables u_image, u_resolution, and v_texCoord that must be passed to params.
+REMOVE ALL INFORMATION OTHER THAN JSON.
+NOTE: when using the a variable as initial value or upper limit of a for loop, the variable should be a constant variable.
 
 ## Format
 
