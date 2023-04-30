ec60ee5b81e286346ecf59955c726e53551a92d1
Itta Funahashi <itta@itta.dev>
2023-04-30 14:06:11 +0900

    feat: update ParamArea styles

--- components/ParamArea.tsx
+++ components/ParamArea.tsx
@@ -9,9 +9,9 @@ interface ParamAreaProps {
 
 const ParamArea: FC<ParamAreaProps> = ({ handleParamChange, params }) => {
   return (
-    <div className="flex">
+    <div className="flex space-x-2">
       {params.map((param, index) => (
-        <div key={index}>
+        <div key={index} className="w-16 space-y-2">
           <Slider
             orientation="vertical"
             defaultValue={[param.value]}
@@ -20,7 +20,7 @@ const ParamArea: FC<ParamAreaProps> = ({ handleParamChange, params }) => {
             step={param.step}
             onValueChange={(value) => handleParamChange(param.name, value[0])}
           />
-          <span className="text-white">{param.label}</span>
+          <div className="text-white">{param.label}</div>
         </div>
       ))}
     </div>
