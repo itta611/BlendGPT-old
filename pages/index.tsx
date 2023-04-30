a46be412ddb7aaecbc7df9e32ec74c2f7d4a9e1a
Itta Funahashi <itta@itta.dev>
2023-04-30 15:23:53 +0900

    feat: add separator

--- pages/index.tsx
+++ pages/index.tsx
@@ -152,6 +152,7 @@ export default function Home() {
               </div>
             )}
           </form>
+          <div className="h-0 border-t border-t-white/20 mx-3"></div>
           {params.length > 0 && <ParamArea params={params} handleParamChange={handleParamChange} />}
         </div>
       </main>
