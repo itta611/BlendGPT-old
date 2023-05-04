df301f291646a07c2b6ddaea63236742d862f607
Itta Funahashi <itta@itta.dev>
2023-05-04 16:59:30 +0900

    feat: @vercel/analyticsを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { CanvasDrawerProvider } from 'hooks/useCanvasDrawer';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CanvasDrawerProvider>
       <Component {...pageProps} />
+      <Analytics />
     </CanvasDrawerProvider>
   );
 }
